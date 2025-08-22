80e8dbdfc78f5d1944255cac4bd0ee87f34bd94e
Raistlinshad <65968903+Raistlinshad@users.noreply.github.com>
2025-08-22 19:25:29 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -25,80 +25,119 @@
 #include <QProcess>
 #include "LaneClient.h"
 #include "QuickGame.h"
+#include "BowlingWidgets.h"
+#include "MediaManager.h"
 
 // Forward declarations
 class BowlingMainWindow;
 
-// Pin display widget
-class PinDisplayWidget : public QWidget {
+
+// Bowler display widget
+class BowlerWidget : public QFrame {
     Q_OBJECT
     
 public:
-    PinDisplayWidget(QWidget* parent = nullptr) : QWidget(parent) {
-        setupUI();
-        resetPins();
+    BowlerWidget(const Bowler& bowler, bool isCurrentPlayer = false, QWidget* parent = nullptr) 
+        : QFrame(parent), bowlerData(bowler) {
+        setupUI(isCurrentPlayer);
+        updateDisplay();
     }
     
-    void setPinStates(const QVector<int>& states) {
-        pinStates = states;
-        updatePinDisplay();
+    void updateBowler(const Bowler& bowler, bool isCurrentPlayer = false) {
+        bowlerData = bowler;
+        updateHighlight(isCurrentPlayer);
+        updateDisplay();
     }
     
-    void resetPins() {
-        pinStates = QVector<int>(5, 1); // All pins up
-        updatePinDisplay();
+    void updateHighlight(bool isCurrentPlayer) {
+        if (isCurrentPlayer) {
+            setStyleSheet("QFrame { background-color: yellow; border: 3px solid red; }");
+        } else {
+            setStyleSheet("QFrame { background-color: lightblue; border: 1px solid black; }");
+        }
     }
 
-private slots:
-    void updatePinDisplay() {
-        // Pin layout: lTwo, lThree, cFive, rThree, rTwo
-        const QStringList pinNames = {"L2", "L3", "C5", "R3", "R2"};
-        
-        for (int i = 0; i < 5; ++i) {
-            QLabel* pinLabel = pinLabels[i];
-            if (pinStates[i] == 0) {
-                // Pin down - black
-                pinLabel->setStyleSheet("QLabel { background-color: black; color: white; border: 2px solid white; }");
-            } else {
-                // Pin up - white
-                pinLabel->setStyleSheet("QLabel { background-color: white; color: black; border: 2px solid black; }");
-            }
-            pinLabel->setText(pinNames[i]);
-        }
-    }
-    
 private:
-    void setupUI() {
+    void setupUI(bool isCurrentPlayer) {
+        setFrameStyle(QFrame::Box);
+        updateHighlight(isCurrentPlayer);
+        
         QGridLayout* layout = new QGridLayout(this);
-        layout->setSpacing(5);
         
-        // Canadian 5-pin layout
-        //     L3
-        //  L2    R2
-        //     C5
-        //     R3
+        // Bowler name
+        nameLabel = new QLabel(bowlerData.name, this);
+        nameLabel->setFont(QFont("Arial", 20, QFont::Bold));
+        nameLabel->setAlignment(Qt::AlignCenter);
+        layout->addWidget(nameLabel, 0, 0, 1, 11);
         
-        pinLabels.resize(5);
-        
-        // Create pin labels
-        for (int i = 0; i < 5; ++i) {
-            pinLabels[i] = new QLabel(this);
-            pinLabels[i]->setAlignment(Qt::AlignCenter);
-            pinLabels[i]->setMinimumSize(40, 60);
-            pinLabels[i]->setMaximumSize(40, 60);
-            pinLabels[i]->setFont(QFont("Arial", 10, QFont::Bold));
+        // Frame headers
+        QStringList headers = {"F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "Total"};
+        for (int i = 0; i < headers.size(); ++i) {
+            QLabel* header = new QLabel(headers[i], this);
+            header->setFont(QFont("Arial", 12, QFont::Bold));
+            header->setAlignment(Qt::AlignCenter);
+            header->setStyleSheet("QLabel { border: 1px solid black; background-color: lightgray; }");
+            layout->addWidget(header, 1, i);
         }
         
-        // Position pins (lTwo=0, lThree=1, cFive=2, rThree=3, rTwo=4)
-        layout->addWidget(pinLabels[1], 0, 1);  // L3 top center
-        layout->addWidget(pinLabels[0], 1, 0);  // L2 left
-        layout->addWidget(pinLabels[4], 1, 2);  // R2 right
-        layout->addWidget(pinLabels[2], 2, 1);  // C5 center
-        layout->addWidget(pinLabels[3], 3, 1);  // R3 bottom center
+        // Frame displays
+        frameLabels.resize(10);
+        totalLabels.resize(10);
+        
+        for (int i = 0; i < 10; ++i) {
+            // Ball results
+            frameLabels[i] = new QLabel(this);
+            frameLabels[i]->setAlignment(Qt::AlignCenter);
+            frameLabels[i]->setStyleSheet("QLabel { border: 1px solid black; background-color: white; }");
+            frameLabels[i]->setMinimumHeight(30);
+            layout->addWidget(frameLabels[i], 2, i);
+            
+            // Frame totals
+            totalLabels[i] = new QLabel(this);
+            totalLabels[i]->setAlignment(Qt::AlignCenter);
+            totalLabels[i]->setStyleSheet("QLabel { border: 1px solid black; background-color: white; }");
+            totalLabels[i]->setFont(QFont("Arial", 14, QFont::Bold));
+            totalLabels[i]->setMinimumHeight(40);
+            layout->addWidget(totalLabels[i], 3, i);
+        }
+        
+        // Total score
+        grandTotalLabel = new QLabel(this);
+        grandTotalLabel->setAlignment(Qt::AlignCenter);
+        grandTotalLabel->setStyleSheet("QLabel { border: 2px solid black; background-color: yellow; }");
+        grandTotalLabel->setFont(QFont("Arial", 24, QFont::Bold));
+        grandTotalLabel->setMinimumHeight(70);
+        layout->addWidget(grandTotalLabel, 2, 10, 2, 1);
     }
     
-    QVector<QLabel*> pinLabels;
-    QVector<int> pinStates;
+    void updateDisplay() {
+        nameLabel->setText(bowlerData.name);
+        
+        for (int i = 0; i < 10; ++i) {
+            const Frame& frame = bowlerData.frames[i];
+            
+            // Update ball display
+            frameLabels[i]->setText(frame.getDisplayText());
+            
+            // Update frame total
+            if (frame.isComplete) {
+                totalLabels[i]->setText(QString::number(frame.totalScore));
+            } else if (!frame.balls.isEmpty()) {
+                totalLabels[i]->setText("...");
+            } else {
+                totalLabels[i]->setText("");
+            }
+        }
+        
+        // Update grand total
+        grandTotalLabel->setText(QString::number(bowlerData.totalScore));
+    }
+    
+    Bowler bowlerData;
+    QLabel* nameLabel;
+    QVector<QLabel*> frameLabels;
+    QVector<QLabel*> totalLabels;
+    QLabel* grandTotalLabel;
 };
 
 // Media display widget for ads and special effects
@@ -189,71 +228,20 @@ private:
     QTimer* mediaTimer;
 };
 
-
-// Machine interface helper
-class MachineInterface : public QObject {
-    Q_OBJECT
-    
-public:
-    MachineInterface(QObject* parent = nullptr) : QObject(parent) {
-        pythonProcess = new QProcess(this);
-        connect(pythonProcess, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
-                this, &MachineInterface::onProcessFinished);
-        connect(pythonProcess, &QProcess::readyReadStandardOutput,
-                this, &MachineInterface::onDataReady);
-    }
-    
-    void startDetection() {
-        if (pythonProcess->state() == QProcess::NotRunning) {
-            pythonProcess->start("python3", QStringList() << "machine_interface.py");
-        }
-    }
-    
-    void stopDetection() {
-        if (pythonProcess->state() != QProcess::NotRunning) {
-            pythonProcess->terminate();
-            if (!pythonProcess->waitForFinished(3000)) {
-                pythonProcess->kill();
-            }
-        }
-    }
-
-signals:
-    void ballDetected(const QVector<int>& pins);
-    void machineError(const QString& error);
-
-private slots:
-    void onProcessFinished(int exitCode, QProcess::ExitStatus exitStatus) {
-        Q_UNUSED(exitStatus)
-        if (exitCode != 0) {
-            emit machineError("Machine process crashed");
-        }
-    }
-    
-    void onDataReady() {
-        QByteArray data = pythonProcess->readAllStandardOutput();
-        QStringList lines = QString::fromUtf8(data).split('\n', Qt::SkipEmptyParts);
-        
-        for (const QString& line : lines) {
-            QJsonDocument doc = QJsonDocument::fromJson(line.toUtf8());
-            if (doc.isObject()) {
-                QJsonObject obj = doc.object();
-                if (obj["type"].toString() == "ball_detected") {
-                    QJsonArray pinsArray = obj["pins"].toArray();
-                    QVector<int> pins;
-                    for (const QJsonValue& value : pinsArray) {
-                        pins.append(value.toInt());
-                    }
-                    if (pins.size() == 5) {
-                        emit ballDetected(pins);
-                    }
-                }
-            }
-        }
-    }
-
 private:
-    QProcess* pythonProcess;
+    MediaDisplayWidget* mediaDisplay;
+    QScrollArea* gameDisplayArea;
+    QWidget* gameWidget;
+    QVBoxLayout* gameLayout;
+    GameStatusWidget* gameStatus;
+    
+    QPushButton* holdButton;
+    QPushButton* skipButton;
+    QPushButton* resetButton;
+    QLabel* scrollLabel = nullptr;
+    
+    LaneClient* client;
+    QuickGame* game;
 };
 
 int main(int argc, char *argv[])
