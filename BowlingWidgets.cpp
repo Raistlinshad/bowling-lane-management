6210694c90e3751ccf98485cd54213f9eda05d02
Raistlinshad <65968903+Raistlinshad@users.noreply.github.com>
2025-08-24 14:45:47 -0400

    Update BowlingWidgets.cpp

--- BowlingWidgets.cpp
+++ BowlingWidgets.cpp
@@ -9,9 +9,9 @@
 // Static constants for PinDisplayWidget - CORRECTED LAYOUT
 const QVector<QPointF> PinDisplayWidget::pinPositions = {
     QPointF(0.15, 0.3),   // lTwo (top-left)
-    QPointF(0.35, 0.15),  // lThree (upper-left) 
-    QPointF(0.5, 0.27),    // cFive (bottom-center)
-    QPointF(0.65, 0.15),  // rThree (upper-right)
+    QPointF(0.35, 0.45),  // lThree (upper-left) 
+    QPointF(0.5, 0.6),    // cFive (bottom-center)
+    QPointF(0.65, 0.45),  // rThree (upper-right)
     QPointF(0.85, 0.3)    // rTwo (top-right)
 };
 
@@ -958,4 +958,5 @@ void ScrollTextWidget::onScrollTimer() {
 void ScrollTextWidget::calculateScrollParameters() {
     QFontMetrics fm(scrollFont);
     textWidth = fm.horizontalAdvance(scrollText);
+
 }
